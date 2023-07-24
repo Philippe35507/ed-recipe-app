f2fc5ea009442ffa8a5dc89e9dbe74a6b88e00f8
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-24 19:18:27 +0100

    Creating Routes
    + <cuisine /> for <Category /> component

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,15 @@
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cuisine" element={<Cuisine />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
