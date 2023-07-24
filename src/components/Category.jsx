a388a456ff3ba4d1a84cf8e917d3dfab07c9f9c7
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-24 19:30:35 +0100

    NavLink
    + moving BrowserRouter from <Pages /> to <App />
    + modification Route /cuisine to avoid error
    "No routes matched location "/cuisine/American" ....

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,26 +1,29 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 function Category() {
   return (
     <List>
-      <div>
-        <FaPizzaSlice />
-        <h4>Italian</h4>
-      </div>
-      <div>
-        <FaHamburger />
-        <h4>American</h4>
-      </div>
-      <div>
-        <GiNoodles />
-        <h4>Thai</h4>
-      </div>
-      <div>
-        <GiChopsticks />
-        <h4>Japanese</h4>
-      </div>
+      <List>
+        <NavLink to={"/cuisine/Italian"}>
+          <FaPizzaSlice />
+          <h4>Italian</h4>
+        </NavLink>
+        <NavLink to={"/cuisine/American"}>
+          <FaHamburger />
+          <h4>American</h4>
+        </NavLink>
+        <NavLink to={"/cuisine/Thai"}>
+          <GiNoodles />
+          <h4>Thai</h4>
+        </NavLink>
+        <NavLink to={"/cuisine/Japanese"}>
+          <GiChopsticks />
+          <h4>Japanese</h4>
+        </NavLink>
+      </List>
     </List>
   );
 }
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,15 +1,13 @@
 import Home from "./Home";
 import Cuisine from "./Cuisine";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function Pages() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cuisine" element={<Cuisine />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/cuisine/:type" element={<Cuisine />}></Route>
+    </Routes>
   );
 }
 
