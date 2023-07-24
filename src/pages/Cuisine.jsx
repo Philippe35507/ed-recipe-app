24f20e87a96fdfc54caff46b12cbef6ab72086db
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-24 19:47:56 +0100

    (Console.log) fetching types of cuisines
      console.log(params.type);
      }, [params.type]);

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,5 +1,24 @@
+import Styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=a1fffd88cfe64158a8c4ce62e1dca228&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+  useEffect(() => {
+    // getCuisine("Italian");
+    console.log(params.type);
+  }, [params.type]);
+  return <div></div>;
 }
 
 export default Cuisine;
