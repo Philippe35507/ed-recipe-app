bdaa41464c868d96bb4e04a789c672962b0bfd5c
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-25 23:40:03 +0100

    Creating <Recipe /> page + dynamic route

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import Home from "./Home";
 import Cuisine from "./Cuisine";
 import { Routes, Route } from "react-router-dom";
 import Searched from "./Searched";
+import Recipe from "./Recipe";
 
 function Pages() {
   return (
@@ -9,6 +10,7 @@ function Pages() {
       <Route path="/" element={<Home />}></Route>
       <Route path="/cuisine/:type" element={<Cuisine />}></Route>
       <Route path="/searched/:search" element={<Searched />}></Route>
+      <Route path="/recipe/:name" element={<Recipe />}></Route>
     </Routes>
   );
 }
